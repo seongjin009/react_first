9a959a3a022125beacf30379d5b46c59806ceec3
seongjin009 <iseongjin009@gmail.com>
2023-09-25 16:15:09 +0900

    각도변경하기

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,19 @@ import './style.scss';
 import { useState } from 'react';
 
 function App() {
+	const [Degree, setDegree] = useState(0);
+
 	//const [상태값,상태변경전용함수] = useState(초기값);
 	//리엑트 컴포넌트는 State값이 state변경함수로 변경되어야지만 컴포넌트가 재랜더링됨
 	//숫자를 증가, 감소 시킬 때 전위증감 연산자를 꺼야지만 해당 랜더링 사이클에서 바로 값이 변경되면서 다음번 랜더링에 반영됨
 	//State에 담기는 값이 바뀔때에만 화면의 갱신이 일어나기 때문에
 	//State에 담기는 데이터만 관리하면 되므로 유지보수가 편함
-	let [Num, setNum] = useState(0);
+
 	return (
 		<>
-			<h1>{Num}</h1>
-			<button onClick={() => setNum(--Num)}>minus</button>
-			<button onClick={() => setNum(++Num)}>plus</button>
+			<button onClick={() => setDegree(Degree - 45)}>왼쪽으로 회전</button>
+			<button onClick={() => setDegree(Degree + 45)}>오른쪽으로 회전</button>
+			<article style={{ transform: `rotate(${Degree}deg)` }}>{Degree}</article>
 		</>
 	);
 }
